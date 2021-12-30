d13011c71d8204a127397893df1f1e301751d9b7
Youssef Oufkir <youssef.oufkir.dev@gmail.com>
2021-12-30 22:02:41 +0100

    UI implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { ReactComponent as MinusButtonIcon } from './assets/minus-button.svg'
+import { ReactComponent as PlusButtonIcon } from './assets/plus-button.svg'
+import { ReactComponent as RedoButtonIcon } from './assets/redo-solid.svg'
+
 
 function App() {
   
@@ -13,37 +21,70 @@ function App() {
     
     counterDisplay: {
       backgroundColor: '#2A2E2F', 
-      height: '45vh', 
+      height: '45vh',
+      position: 'relative',
+      display: 'flex'
+    },
+
+    counterNumber: {
+      color: 'white', 
+      fontWeight: 700, 
+      fontSize: '125px',
+      width: '100%', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center'
+    },
+
+    resetCounterButton: {
+      position: 'absolute', 
+      right: '0px', 
+      top: '0px', 
+      margin: '12px 12px 0 0'
+    },
+
+    counterButtonsPaper: {
+      backgroundColor: '#2A2E2F', 
+      height: '25vh', 
     },
 
     counterButtons: {
-      backgroundColor: '#2A2E2F', 
-      height: '25vh', 
+      height: '100%', 
+      width: '100%'
     }
   }
 
   return (
-    <div className="App">
+    <Box>
       <Grid container spacing={1} sx={styles.container}>   
         <Grid item xs={12}>
           <Paper elevation={3} sx={styles.counterDisplay}>
-        
+            <IconButton sx={styles.resetCounterButton}>
+              <RedoButtonIcon className="resetCounterButtonIcon"/>
+            </IconButton>
+            <Typography variant="h1" component="h1" sx={styles.counterNumber}>
+                12
+            </Typography>
           </Paper>
         </Grid>
         <Grid item container spacing={1}>
           <Grid item xs={6}>
-              <Paper elevation={3} sx={styles.counterButtons}>
-          
+              <Paper elevation={3} sx={styles.counterButtonsPaper}>
+                <Button sx={styles.counterButtons}>
+                  <MinusButtonIcon className="counterButtonsIcon"/>
+                </Button>
               </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Paper elevation={3} sx={styles.counterButtons}>
-        
+            <Paper elevation={3} sx={styles.counterButtonsPaper}>
+              <Button sx={styles.counterButtons}>
+                  <PlusButtonIcon className="counterButtonsIcon"/>
+              </Button>
             </Paper>
           </Grid>
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 }
 
